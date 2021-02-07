1e485314daf40de28ae479548ab946de73dc4dcc
farahali3 <72891595+farahali3@users.noreply.github.com>
2021-02-07 17:21:11 -0500

    quote app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dig4639-ex2
+
+"There is a time for many words, and there is also time for sleep."
+        -- Homer, The Odyssey,`
+        `"Sleep, delicious and profound, the very counterfeit of death."
+        -- Homer, The Odyssey,`
+        `"Veni, vidi, vici"
+        -- Julius Caesar, in a letter to the Roman Senate,
---
+++ index.js
@@ -0,0 +1,11 @@
+let quotes = [
+    `"There is a time for many words, and there is also time for sleep."
+    -- Homer, The Odyssey`,
+    `"Sleep, delicious and profound, the very counterfeit of death."
+    -- Homer, The Odyssey`,
+    `"Veni, vidi, vici"
+    -- Julius Caesar, in a letter to the Roman Senate,`,
+]
+
+let index= Math.floor(Math.random() * 2 )
+console.log(quotes[index])
